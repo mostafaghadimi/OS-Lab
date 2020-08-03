c96c302450ed2e84c5704cda14368a0f4ad56110
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-08-03 23:26:48 +0430

    [report2] access corrected

--- 2/practical/access.cpp
+++ 2/practical/access.cpp
@@ -13,7 +13,28 @@ int main(int argc, char *argv[])
     }
     else
     {
-        printf("%s doesn't exist.", path);
+        printf("%s doesn't exist\n", path);
+    }
+    result = access(path, R_OK);
+    if (result == 0) {
+        printf("read permission is granted\n");
+    }
+    else {
+        printf("read permission isn't granted\n");
+    }
+    result = access(path, W_OK);
+    if (result == 0) {
+        printf("write permission is granted\n");
+    }
+    else {
+        printf("write permission isn't granted\n");
+    }
+    result = access(path, X_OK);
+    if (result == 0) {
+        printf("execute permission is granted\n");
+    }
+    else {
+        printf("execute permission isn't granted\n");
     }
     return 0;
 }
